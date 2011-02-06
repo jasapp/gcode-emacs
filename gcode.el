b02d1cd7cd816a03ea9ef20dfa889128964d91ac
Jeff Sapp <jasapp@gmail.com>
2011-02-05 20:25:34 -0600

    Let's see if this works. Still having trouble with comments.

--- gcode.el
+++ gcode.el
@@ -4,10 +4,9 @@
 ;;
 
 (defvar gcode-mode-hook nil
-  "*List of functions to call when entering GCode mode.*")
+  "*List of functions to call when entering gcode mode.*")
 
-(defvar gcode-mode-map nil
-  "Keymap for major mode.")
+(defvar gcode-mode-map (make-keymap))
 
 (defvar gcode-font-lock-keywords
   (list '("\\<\\([gmtGMT][0-9]\\{2\\}\\)\\>" . font-lock-function-name-face)
@@ -15,25 +14,29 @@
 		'("\\<\\([A-Z][+-]?[0-9]+\\(\\.[0-9]+\\)?\\)\\>" . font-lock-keyword-face)
 		))
 
-(defun gcode-comment-dwim (arg)
+(defun xx-comment-dwim (arg)
 "Comment or uncomment current line or region in a smart way.
 For detail, see `comment-dwim'."
    (interactive "*P")
    (require 'newcomment)
-   (let ((deactivate-mark nil) 
-	 (comment-start "(") 
-	 (comment-end ")"))
+   (let ((deactivate-mark nil) (comment-start "#") (comment-end ""))
      (comment-dwim arg)))
 
-(defun gcode-mode () 
+(define-derived-mode gcode-mode fundamental-mode
   "Major mode for editing gcode."
-  (interactive)
-  (kill-all-local-variables)
-  (text-mode)
+  ;; (interactive)
+  ;; (kill-all-local-variables)
   (setq major-mode 'gcode-mode)
   (setq mode-name "Gcode")
+
+  ;; setup our fonts
   (set (make-local-variable 'font-lock-defaults) 
        '(gcode-font-lock-keywords))
+
+  ;; setup our comments
+  (define-key gcode-mode-map [remap comment-dwim] 'xx-comment-dwim)
+  (modify-syntax-entry ?# "< b" xx-mode-syntax-table)
+  (modify-syntax-entry ?\n "> b" xx-mode-syntax-table)
 
   (run-hooks 'gcode-mode-hook))
 
